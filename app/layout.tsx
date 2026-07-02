5b8dbda0579d763142788729810bcae4ab82acf5
andressuarez102001 <andressuarez2910@gmail.com>
2026-07-02 16:10:12 -0500

    fix: forzar fondo negro en moviles

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
-      <body className="bg-black text-white min-h-screen flex flex-col justify-between">
+    // SOLUCIÓN 1: Le metemos bg-black a la etiqueta html para blindar el fondo de los navegadores móviles
+    <html lang="es" className="bg-black">
+      {/* SOLUCIÓN 2: Añadimos bg-black por seguridad también en el body */}
+      <body className="bg-black text-white min-h-screen flex flex-col justify-between antialiased">
+        
         {/* El Navbar se renderiza de forma global aquí arriba */}
         <Navbar />
         
-        <div className="flex-grow">
+        {/* SOLUCIÓN 3: Forzamos bg-black en el contenedor que envuelve todas tus pantallas */}
+        <div className="flex-grow bg-black">
           {children} {/* Aquí Next.js inyectará dinámicamente el Home o el Login */}
         </div>
 
